00a0edb0566d1c11a1006fa4f269618bab8993c6
Tom PoLáKoSz <polakosz@freemail.hu>
2020-06-15 21:33:32 +0200

    Add navbar to the top of the page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import styled from "styled-components";
 import "./App.css";
 import OfflineCardService from "./services/OfflineCardService";
 import LiveCardService from "./services/LiveCardService";
-import { Container, Col, Row } from "react-bootstrap";
+import {
+  Container,
+  Col,
+  Button,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Row,
+} from "react-bootstrap";
 
 const App = (props) => {
   const [loadedCards, setLoadedCards] = useState([]);
@@ -15,21 +25,39 @@ const App = (props) => {
       setLoadedCards(cards);
       console.log(cards);
     });
+
+    new IntersectionObserver(function (e, o) {
+      if (e[0].intersectionRatio > 0) {
+        document.documentElement.removeAttribute("class");
+      } else {
+        document.documentElement.setAttribute("class", "stuck");
+      }
+    }).observe(document.querySelector(".trigger"));
   }, []);
 
   return (
-    <Container>
-      <Row className="d-flex flex-wrap">
-        {loadedCards.map((card) => (
-          <Col key={card.multiverseid} xs={4} md={3} className="p-3">
-            <p>
-              {card.name}, multiverseid: {card.multiverseid}
-            </p>
-            <img src={card.imageUrl} className="img-fluid" alt="Card"></img>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <div>
+      <span className="position-absolute trigger"></span>
+      <Navbar expand="lg" className="sticky-top">
+        <Container>
+          <Navbar.Brand href="#" className="mx-auto">
+            Magic: The Gathering
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row className="d-flex flex-wrap">
+          {loadedCards.map((card) => (
+            <Col key={card.multiverseid} xs={4} md={3} className="p-3">
+              <p>
+                {card.name}, multiverseid: {card.multiverseid}
+              </p>
+              <img src={card.imageUrl} className="img-fluid" alt="Card"></img>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
   );
 };
 
