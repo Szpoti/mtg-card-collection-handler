378038c170b3c50cf0c4e4079b2959496d29e652
Farago Balazs <balage.farago@gmail.com>
2020-06-15 12:37:00 +0200

    basic initializing of mtg api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import "./App.css";
 
-function App() {
+const App = (props) => {
+  const mtg = require("mtgsdk");
+
+  const [loadedCards, setLoadedCards] = useState([]);
+
+  useEffect(() => {
+    console.log("In use effect");
+    mtg.card
+      .where({
+        supertypes: "legendary",
+        subtypes: "demon",
+        types: "creature",
+        colors: "black",
+        page: 0,
+      })
+      .then((cards) => {
+        return cards.filter((card) => card.multiverseid !== undefined);
+      })
+      .then((cards) => {
+        setLoadedCards(cards);
+        console.log(cards);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={parentDiv}>
+      {loadedCards.map((card) => (
+        <div key={card.multiverseid} style={child}>
+          <p>
+            {card.name}, multiverseid: {card.multiverseid}
+          </p>
+          <img src={card.imageUrl} alt="Missing"></img>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+const parentDiv = {
+  display: "flex",
+  flexWrap: "wrap",
+};
+
+const child = {
+  fontSize: "150%",
+  flex: "1 0 21%",
+  margin: "5px",
+};
 
 export default App;
