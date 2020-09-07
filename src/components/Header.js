e4652fb72285eff8faa5d8782771250861e0e621
Tom PoLáKoSz <polakosz@freemail.hu>
2020-09-07 23:49:33 +0200

    Decrease navbar jumping

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navbar, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const navBar = React.createRef();
+  const handleScroll = () => {
+      if (50 < window.pageYOffset) {
+        navBar.current.classList.add('unscrolled');
+        navBar.current.classList.remove('scrolled');
+      } else {
+        navBar.current.classList.add('scrolled');
+        navBar.current.classList.remove('unscrolled');
+      }
+  };
+
+  useEffect(() => {
+      window.addEventListener('scroll', handleScroll, {
+        passive: true
+      });
+
+      return () => {
+          window.removeEventListener('scroll', handleScroll);
+      };
+  }, []);
+
   return (
-    <div>
-      <span className="position-absolute trigger"></span>
-      <Navbar expand="lg" className="sticky-top">
+    <div className="animated-navbar unscrolled" ref={navBar}>
+      <Navbar expand="lg" className="fixed-top">
         <Container>
           <Navbar.Brand className="mx-auto">
             <Link to="/">Magic: The Gathering</Link>
