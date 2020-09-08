8680b8861d8e082e3706ac3c5d7de8134b8144be
Tom PoLáKoSz <polakosz@freemail.hu>
2020-09-08 11:05:05 +0200

    Remove animation from NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,12 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>
-      <span className="position-absolute trigger"></span>
-      <Navbar expand="lg" className="sticky-top">
-        <Container>
-          <Navbar.Brand className="mx-auto">
-            <Link to="/">Magic: The Gathering</Link>
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br></br>
-    </div>
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand className="mx-auto">
+          <Link to="/">Magic: The Gathering</Link>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
   );
 }
